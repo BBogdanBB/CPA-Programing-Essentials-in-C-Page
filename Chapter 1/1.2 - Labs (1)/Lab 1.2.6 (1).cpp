df0ca7e771239902e8d034b29a3c88030e192e04
Bogdan Badun <bogdanfirst0@gmail.com>
2017-11-06 14:32:32 +0200

    Commit Chapter 1

---
+++ Chapter 1/1.2 - Labs (1)/Lab 1.2.6 (1).cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+	cout << "Hello, dear friend1\nHello, dear friend2\n";
+	cout << "Hello, dear friend3";
+	cout << endl;
+	cout << "It's me, your first program.";
+return 0;
+}
---
+++ Chapter 1/1.7 - Labs (3)/Lab 1.7.5 (3).cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int x1, x2, x3, x4;
+	cin>>x1>>x2>>x3>>x4;
+	cout<<x1<<"."<<x2<<"."<<x3<<"."<<x4;
+}
